973764646507234e31d1c3fe983d0c770a0764b3
nicholascm90 <nicholascm90@gmail.com>
2015-10-26 22:23:14 -0400

    Website commit
    Initial website commit.

---
+++ js/index.js
@@ -0,0 +1,21 @@
+/*$(document).ready(function() {
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var offsetTop = $('.inactiveIcon').offset().top;
+    var offsetTop2 = $('.inactiveIconLevel').offset().top; 
+        if($(window).scrollTop() > offsetTop-420) 
+          {       
+            $(".inactiveIcon").addClass("animated zoomOutUp");
+          }
+          
+    /*if (scroll >= 250) {
+      $(".inactiveIcon").addClass("opaque");
+    } else if (scroll > 300) {
+      $(".inactiveIconLevel2").addClass("opacity"); 
+    }else {
+    }
+  }); 
+});
+
+*/
